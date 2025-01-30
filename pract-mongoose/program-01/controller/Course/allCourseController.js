2c0523a5461bcad8fe4577c40a63d74694b05bbb
Wasim Hossain <wasim.hossain003@gmail.com>
2025-01-30 14:29:39 +0600

    exercise two done

--- pract-mongoose/program-01/controller/Course/allCourseController.js
+++ pract-mongoose/program-01/controller/Course/allCourseController.js
@@ -1,8 +1,8 @@
 const Course = require('../../model/courseSchema')
 
 const allCourseController = async (req, res)=>{
-    const getcourse = await Course.find({"isPublish":true, "tags":/.*backend.*/i}).sort("-name").select('name author')
+    const getcourse = await Course.find({"isPublish":true, "tags": {$in:["frontend", "backend"]}}).sort("-price").select('name author')
     res.send(getcourse)
 }
-
+ 
 module.exports = allCourseController
