e929304c16f4e4e7fc25f4d8f4d9236d9b43384c
Wasim Hossain <wasim.hossain003@gmail.com>
2025-01-31 07:21:20 +0600

    Update upadateCourseController.js

--- pract-mongoose/program-01/controller/Course/upadateCourseController.js
+++ pract-mongoose/program-01/controller/Course/upadateCourseController.js
@@ -1,9 +1,20 @@
+const  mongoose = require('mongoose');
 const Course = require('../../model/courseSchema');
 
 const updateCorseController = async (req,res)=>{
     const {id}= req.body
-    const course = await Course.findByIdAndUpdate({"_id":id},{""})
-    res.send(id)
+    // const findCourse = await Course.findById({"_id": id})
+    const course = await Course.findOneAndUpdate(
+        {"_id":id , "isPublish":false},
+        {$set:{"name": "React.js"}},
+        {new: true}
+    )
+    if(course){
+        res.send(course)
+
+    }else{
+        res.send("Course not found.")
+    }
 }
 
 module.exports = updateCorseController;
