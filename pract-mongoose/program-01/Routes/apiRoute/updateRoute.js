4a7b02d9dc0129b11f67f978f36a656937978578
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-02 16:42:58 +0600

    practice with setOnInsert method

--- pract-mongoose/program-01/Routes/apiRoute/updateRoute.js
+++ pract-mongoose/program-01/Routes/apiRoute/updateRoute.js
@@ -3,11 +3,13 @@ const renameController = require('../../controller/Course/updatePart/renameContr
 const incController = require('../../controller/Course/updatePart/incController')
 const maxController = require('../../controller/Course/updatePart/maxController')
 const mulController = require('../../controller/Course/updatePart/mulController')
+const setOnInsertController = require('../../controller/Course/updatePart/setOnInsertController')
 const _ = express.Router()
 
 _.post("/rename", renameController)
 _.post("/inc", incController)
 _.post("/max", maxController)
 _.post("/mul", mulController)
+_.post("/setoninsert", setOnInsertController)
 
 module.exports = _
---
+++ pract-mongoose/program-01/controller/Course/updatePart/setOnInsertController.js
@@ -0,0 +1,14 @@
+const Course = require('../../../model/courseSchema')
+
+const setOnInsertController = async (req,res)=>{
+    const { courseStatus}= req.body
+    const course = await Course.updateMany(
+        {$and:[{"author": /.*Mosh.*/},{"price":{$lt:20}}] },
+        {$set:{"status": courseStatus}},
+        {upsert:false, writeConcern: { w: "majority" }},
+    )
+
+    res.send(course)
+}
+
+module.exports = setOnInsertController
