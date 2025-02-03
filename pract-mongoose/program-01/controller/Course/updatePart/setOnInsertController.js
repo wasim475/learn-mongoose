a1dc2d06eb5352a472c55ab11d89aa353491a5d2
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-03 07:30:27 +0600

    create upsert txt file

--- pract-mongoose/program-01/controller/Course/updatePart/setOnInsertController.js
+++ pract-mongoose/program-01/controller/Course/updatePart/setOnInsertController.js
@@ -2,7 +2,7 @@ const Course = require('../../../model/courseSchema')
 
 const setOnInsertController = async (req,res)=>{
     const { courseStatus}= req.body
-    const course = await Course.updateMany(
+    const course = await Course.updateOne(
         {$and:[{"author": /.*Mosh.*/},{"price":{$lt:20}}] },
         {$set:{"status": courseStatus}},
         {upsert:false, writeConcern: { w: "majority" }},
