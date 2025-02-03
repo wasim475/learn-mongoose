2c7286b8735a309c1305dd68447e4084e7665fc2
Wasim Hossain <wasim.hossain003@gmail.com>
2025-02-03 08:33:30 +0600

    Update setOnInsertController.js
    এই কসেপ্টটি এখনো ক্লিয়ার নয় আমার কাছে... এটি নিয়ে আরও ভাবতে হবে... এই কোড সঠিক নয় ... কাজ করছে না

--- pract-mongoose/program-01/controller/Course/updatePart/setOnInsertController.js
+++ pract-mongoose/program-01/controller/Course/updatePart/setOnInsertController.js
@@ -1,13 +1,11 @@
 const Course = require('../../../model/courseSchema')
-
+ 
 const setOnInsertController = async (req,res)=>{
     const {courseId, courseStatus}= req.body
-    const existing = await Course.findOne({"_id": courseId})
-    console.log(existing?.status)
     const course = await Course.updateOne(
-        {"_id":courseId },
-        {$set:{"status": courseStatus}},
-        {upsert:true, writeConcern:{w: "majority"}, strict:false}
+        {$and:[{"_id":courseId},{"price":{$lt:20}}] },
+        {$setOnInsert:{"Course": courseStatus}},
+        {upsert:true, writeConcern:{w: "majority" },strict:false}
     )
 
     res.send(course)
